b8af6e7ed5bb4bf6aab7d42e116a2b12c99d23ee
Ilham202 <42730789+Ilham202@users.noreply.github.com>
2019-04-03 22:00:30 -0600

    Update program1.c

--- program1.c
+++ program1.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
-int main(){
-
+int main()
+ {
 	int bt[10]={0},at[10]={0},tat[10]={0},wt[10]={0},ct[10]={0};
 	int n,sum=0;
 	float totalTAT=0,totalWT=0;
 
-	printf("Enter number of processes	");
+	printf("Enter number of processes");
 	scanf("%d",&n);
 
 	printf("Enter arrival time and burst time for each process\n\n");
@@ -14,10 +14,10 @@ int main(){
 	for(int i=0;i<n;i++)
 	{
 
-		printf("Arrival time of process[%d]	",i+1);
+		printf("Arrival time of process[%d]",i+1);
 		scanf("%d",&at[i]);
 		
-		printf("Burst time of process[%d]	",i+1);
+		printf("Burst time of process[%d]",i+1);
 		scanf("%d",&bt[i]);
 		
 		printf("\n");
