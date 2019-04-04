9d0ff7a8eec9216a22444e521b7dbb9255d69ac6
Ilham202 <42730789+Ilham202@users.noreply.github.com>
2019-04-03 22:39:56 -0600

    Update program1.c

--- program1.c
+++ program1.c
@@ -1,59 +1,48 @@
 #include<stdio.h>
-
 int main()
  {
+	int i,j,k;
 	int bt[10]={0},at[10]={0},tat[10]={0},wt[10]={0},ct[10]={0};
 	int n,sum=0;
 	float totalTAT=0,totalWT=0;
-
-	printf("Enter number of processes");
+        printf("\t\t\t\t!!!!------- FCFS Algorithm ------------!!!!\n\n");
+	printf("Enter number of processes -->");
 	scanf("%d",&n);
-
-	printf("Enter arrival time and burst time for each process\n\n");
-
-	for(int i=0;i<n;i++)
+	printf("Enter arrival time and burst time for each process :: \n\n");
+	for(i=0;i<n;i++)
 	{
-
-		printf("Arrival time of process[%d]",i+1);
+		printf("Arrival time of process[%d] -> ",i+1);
 		scanf("%d",&at[i]);
-		
-		printf("Burst time of process[%d]",i+1);
+		printf("Burst time of process[%d]  -> ",i+1);
 		scanf("%d",&bt[i]);
-		
 		printf("\n");
 	}
-		
-	//calculate completion time of processes 	
-
-	for(int j=0;j<n;j++)
+	
+	//To calculate Completion time of processes 	
+	for(j=0;j<n;j++)
 	{
 		sum+=bt[j];
 		ct[j]+=sum;
 	}
-
-	//calculate turnaround time and waiting times 
-
-	for(int k=0;k<n;k++)
+	//To calculate Turnaround time 
+	for(k=0;k<n;k++)
 	{
 		tat[k]=ct[k]-at[k];
 		totalTAT+=tat[k];
 	}
-
-	
-	for(int k=0;k<n;k++)
+       //To calculate Waiting time
+	for(k=0;k<n;k++)
 	{
 		wt[k]=tat[k]-bt[k];
 		totalWT+=wt[k];
 	}
-	
 	printf("Solution: \n\n");
 	printf("P#\t AT\t BT\t CT\t TAT\t WT\t\n\n");
 	
-	for(int i=0;i<n;i++)
+	for(i=0;i<n;i++)
 	{
-		printf("P%d\t %d\t %d\t %d\t %d\t %d\n",i+1,at[i],bt[i],ct[i],tat[i],wt[i]);
+	printf("P%d\t %d\t %d\t %d\t %d\t %d\n",i+1,at[i],bt[i],ct[i],tat[i],wt[i]);
 	}
-		
 	printf("\n\nAverage Turnaround Time = %f\n",totalTAT/n);
 	printf("Average WT = %f\n\n",totalWT/n);
 	
